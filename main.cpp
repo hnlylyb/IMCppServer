f74e75af7debcafcc799450736b7b3254e9eb59b
killer13 <hnlylyb@gmail.com>
2019-03-06 18:24:48 +0800

    fix bugs. change socket handle mode.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <pthread.h>
 #include <algorithm>
 #include <vector>
+#include <list>
 #include <unordered_map>
 #include <json/json.h>
 
@@ -15,7 +16,6 @@ using namespace std;
 struct ThreadArg
 {
     int epoll_fd = 0;
-    int connnections = 0;
     int thread_id = 0;
 };
 
@@ -25,7 +25,11 @@ struct UserInfo
     int fd = 0;
     bool write_ready = false;
 };
+
 unordered_map<int,UserInfo> g_Infos;
+unordered_map<int,list<std::string> > g_BlockMessages;
+unordered_map<int,int> g_FdId;
+
 int g_mid = 1;
 
 class Thread
@@ -62,18 +66,17 @@ void* deal(void* arg)
         }
         for (int i=0;i!=nfds;i++)
         {
-            printf ("thread: %d, connections: %d, event: %x\n",targ.thread_id,targ.connnections,events[i].events);
+            printf ("thread: %d, event: %x\n",targ.thread_id,events[i].events);
             if ((events[i].events & EPOLLRDHUP) != 0)
             {
                 epoll_event ev;
                 ev = events[i];
                 epoll_ctl(epollfd,EPOLL_CTL_DEL,events[i].data.fd,&ev);
-                targ.connnections--;
                 cout << "disconnected by clinet." << std::endl;
             }
             else if ((events[i].events & EPOLLIN) != 0)
             {
-                cout << "EPOLLIN!" << std::endl;
+                cout << "EPOLLIN! thread id:" << targ.thread_id << std::endl;
                 int num = recv(events[i].data.fd,buf,1024,0);
                 buf[num] = 0;
                 if (strcmp(buf,"bye\n") == 0 || strcmp(buf,"bye\r\n") == 0)
@@ -82,7 +85,6 @@ void* deal(void* arg)
                     ev = events[i];
                     epoll_ctl(epollfd,EPOLL_CTL_DEL,events[i].data.fd,&ev);
                     close(events[i].data.fd);
-                    targ.connnections--;
                     break;
                 }
                 else
@@ -90,19 +92,70 @@ void* deal(void* arg)
                     Json::Reader reader;
                     Json::Value root;
                     string str(buf);
-                    reader.parse(str,root);
+                    if (!reader.parse(str,root))
+                    {
+                        cout << "parse failed! thread id:" << targ.thread_id << std::endl;
+                        continue;
+                    }
                     int id = root["id"].asInt();
                     string messages = root["message"].asString();
-                    send(g_Infos[id].fd,messages.c_str(),messages.length(),0);
+                    if(send(g_Infos[id].fd,messages.c_str(),messages.length(),0) == -1)
+                    {
+                        if (errno == EWOULDBLOCK)
+                        {
+                            g_Infos[id].write_ready = false;
+                            g_BlockMessages[id].emplace_front(messages);
+                        }
+                        else
+                        {
+                            cout << "error in 107 send errno " << errno << "!" << endl;
+                        }
+                        
+                    }
                 }
             }
             else if ((events[i].events & EPOLLOUT) != 0)
             {
-                cout << "user_id "<< g_mid << " login." << std::endl;
-                g_Infos[g_mid].fd = events[i].data.fd;
-                g_Infos[g_mid].user_id = g_mid;
-                g_Infos[g_mid].write_ready = true;
-                g_mid++;
+                if (g_BlockMessages.find(g_FdId[events[i].data.fd]) != g_BlockMessages.end())
+                {
+                    int id = g_FdId[events[i].data.fd];
+                    if (g_Infos[id].write_ready == false)
+                    {
+                        g_Infos[id].write_ready = true;
+                        while (g_BlockMessages[id].begin() != g_BlockMessages[id].end())
+                        if(send(g_Infos[id].fd,g_BlockMessages[id].back().c_str(),g_BlockMessages[id].back().length(),0) == -1)
+                        {
+                            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                            {
+                                g_Infos[id].write_ready = false;
+                                break;
+                            }
+                            else
+                            {
+                                cout << "error in 131 send errno " << errno << "!" << endl;
+                            }
+                        }
+                        else
+                        {
+                            g_BlockMessages[id].pop_back();
+                        }
+                        
+                    }
+                    else
+                    {
+                        cout << "error! g_BlockMessages.find(g_FdId[events[i].data.fd]) != g_BlockMessages.end() but g_Infos[g_FdId[events[i].data.fd]].write_ready != false" << endl;
+                    }
+                    
+                }
+                else
+                {
+                    cout << "user_id "<< g_mid << " login." << std::endl;
+                    g_Infos[g_mid].fd = events[i].data.fd;
+                    g_Infos[g_mid].user_id = g_mid;
+                    g_Infos[g_mid].write_ready = true;
+                    g_FdId[events[i].data.fd] = g_mid;
+                    g_mid++;
+                }
             }
         }
     }
@@ -120,11 +173,12 @@ int main()
     addr.sin_port = htons(10004);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);//inet_addr("0.0.0.0");
     epoll_event ev;
+    int epoll_fd = epoll_create1(0);
     
     ThreadArg arg[8];
     for (int i = 0;i!= 8;i++)
     {
-        arg[i].epoll_fd = epoll_create1(0);
+        arg[i].epoll_fd = epoll_fd;
         arg[i].thread_id = i;
         if (arg[i].epoll_fd == -1)
         {
@@ -157,21 +211,10 @@ int main()
         }
         ev.events = EPOLLIN | EPOLLET | EPOLLRDHUP | EPOLLOUT;
         ev.data.fd = connfd;
-        int idle_thread = 0;
-        for (int i = 0; i != 8; i++)
-        {
-            if (arg[i].connnections < arg[idle_thread].connnections)
-                idle_thread = i;
-        }
-        if (epoll_ctl(arg[idle_thread].epoll_fd, EPOLL_CTL_ADD, connfd, &ev) == -1) 
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connfd, &ev) == -1) 
         {
             cout << "error epoll_ctl."<< errno << endl;
-        }
-        else
-        {
-            arg[idle_thread].connnections++;
-        }
-        
+        }       
     }
     close(sock);
     return 0;
